1326011f23bffc0f1a9fa2f89c053b47af025f01
Diego_M <diegofernandomzlz@gmail.com>
2022-05-30 14:28:14 +0000

    landing-page-w-react

---
+++ src/js/component/card.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+export const Card = () => {
+	return (
+		<>
+			<div className="d-flex container-fluid  w-75">
+				<div className="col">
+					<div className="card">
+						<img
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAFVBMVEX+1QABW7sBXLgAVLqRnmP/2QABW774beLwAAAA4ElEQVR4nO3QsQ2DABDAwIcQ9h85/btFIsXdBJbnYJu3A/6QJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJzck2N9t82OZimy/bAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPOQH0+B0BfnukJ4AAAAASUVORK5CYII="
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a longer card with supporting text below
+								as a natural lead-in to additional content. This
+								content is a little bit longer.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card">
+						<img
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAFVBMVEX+1QABW7sBXLgAVLqRnmP/2QABW774beLwAAAA4ElEQVR4nO3QsQ2DABDAwIcQ9h85/btFIsXdBJbnYJu3A/6QJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJzck2N9t82OZimy/bAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPOQH0+B0BfnukJ4AAAAASUVORK5CYII="
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a longer card with supporting text below
+								as a natural lead-in to additional content. This
+								content is a little bit longer.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card">
+						<img
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAFVBMVEX+1QABW7sBXLgAVLqRnmP/2QABW774beLwAAAA4ElEQVR4nO3QsQ2DABDAwIcQ9h85/btFIsXdBJbnYJu3A/6QJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJzck2N9t82OZimy/bAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPOQH0+B0BfnukJ4AAAAASUVORK5CYII="
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a longer card with supporting text below
+								as a natural lead-in to additional content.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card">
+						<img
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAFVBMVEX+1QABW7sBXLgAVLqRnmP/2QABW774beLwAAAA4ElEQVR4nO3QsQ2DABDAwIcQ9h85/btFIsXdBJbnYJu3A/6QJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJeVKelCflSXlSnpQn5Ul5Up6UJ+VJzck2N9t82OZimy/bAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPOQH0+B0BfnukJ4AAAAASUVORK5CYII="
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a longer card with supporting text below
+								as a natural lead-in to additional content. This
+								content is a little bit longer.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
